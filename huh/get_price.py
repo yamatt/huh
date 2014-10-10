d4fb1893688f9176e2cdeb9e6c096294131bab8a
Matt Copperwaite <mattcopp+git@gmail.com>
2014-10-10 19:04:38 +0100

    moved command in to scripts directory

--- huh/get_price.py
+++
@@ -1,9 +0,0 @@
-import response
-import sys
-
-def search(value):
-    for result in response.responses(value):
-        print result.name, result.price.value, result.price.currency
-        
-if __name__ == "__main__":
-    search(sys.argv[1])
