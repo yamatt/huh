3891af47fc4deb334c35626ebf2f330be53d66cf
Matt Copperwaite <mattcopp+git@gmail.com>
2014-10-13 20:17:35 +0100

    starting next release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages
 
 setup(
     name = 'HardUpHumble',
-    version = '0.1',
+    version = '0.2',
     description = 'HardUpHumble is a program that allows you to monitor the cost of games.',
     author = 'Matt Copperwaite',
     author_email = 'matt@copperwaite.net',
